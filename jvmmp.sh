4b60227e8c081bed6f9b8ee366ea3f3d8c0c24a8
Eduardo7881 <166447787+Eduardo7881@users.noreply.github.com>
2024-05-25 22:21:16 -0300

    Create jvmmp.sh

---
+++ jvmmp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "Verifying file existence..."
+
+# ---- Configuring JavaVM options (like to use memory allocated) ----
+JVMOpts="-Xmx1024M -Xms512M"
+file="nbsjava.jar"
+
+if [ -f "$file" ]; then
+    echo "File exists. Checking Java installation..."
+    java --version > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "Java installed!"
+        echo "Allocated Java Memory: 1 Gigabyte"
+        echo "If you want to change it, change in this file."
+        echo "The Program is ready to start! Press any key to start"
+        read -n 1 -s
+        clear
+        echo "Executing program..."
+        java $JVMOpts -jar "$file"
+        echo -e "\n\nProgram execution completed!"
+        echo "Press any key to exit"
+        read -n 1 -s
+    else
+        echo "Java not installed."
+        echo "Java is the REQUIRED program to run this."
+        echo "Install at least Java 8."
+        echo "Press any key to exit."
+        read -n 1 -s
+        exit 1
+    fi
+else
+    echo "The file $file does not exist."
+    exit 1
+fi
